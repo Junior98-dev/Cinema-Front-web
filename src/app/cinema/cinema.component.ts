f7c7ffbd0700c44c11025ac2461fe93d95b73f5e
Junior98-dev <kounkouddorianjunior@gmail.com>
2023-08-19 11:51:22 +0200

    methode ngOnInit, recupération liste villes

--- src/app/cinema/cinema.component.ts
+++ src/app/cinema/cinema.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cinema',
   templateUrl: './cinema.component.html',
   styleUrls: ['./cinema.component.css']
 })
-export class CinemaComponent {
+export class CinemaComponent implements OnInit
+{
+
+  public villes:any;
+
+  constructor(private http:HttpClient){}
+
+
+  ngOnInit(): void {
+    //throw new Error('Method not implemented.');
+    this.http.get("http://localhost:8286/villes").subscribe(
+      data => {
+        this.villes=data;
+      },err => {
+        console.log(err);
+      }
+    )
+    
+  }
 
 }
